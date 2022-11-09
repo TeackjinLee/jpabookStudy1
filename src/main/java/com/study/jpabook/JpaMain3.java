01293e71b9daaf28ad608632755f8cbf3e67a753
itaegjin <tag101@hanmail.net>
2022-11-09 17:53:52 +0900

    영속성 컨텍스트의 특징 - 엔티티 수정

---
+++ src/main/java/com/study/jpabook/JpaMain3.java
@@ -0,0 +1,28 @@
+package com.study.jpabook;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain3 {
+
+    public static void main(String[] args) {
+        //엔티티 매니저 팩토리 생성
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpabook");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();    // [트랜잭션] 시작
+
+        // 영속 엔티티 데이터 수정
+        Member memberA = em.find(Member.class, "1");
+
+        // 영속 엔티티 데이터 수정
+        memberA.setUsername("hi");
+        memberA.setAge(10);
+
+        //em.update(member) 이런 코드가 있어야 하지 않을까?
+
+        transaction.commit(); // [트랜잭션] 커밋
+    }
+}
