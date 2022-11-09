2f6fb22529dddc4575e9391b3dd1745d7ce249f0
이택진 <itaegjin@itaegjin-ui-MacBookPro.local>
2022-11-09 15:26:58 +0900

    jpabookStudy1

---
+++ src/test/java/com/study/jpabook/JpabookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.jpabook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpabookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
