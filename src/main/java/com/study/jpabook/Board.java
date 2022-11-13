51a85d3058dcad45836f52b3c8ca741054e33855
itaegjin <tag101@hanmail.net>
2022-11-13 15:48:24 +0900

    엔티티매핑 - @Enumerated

--- src/main/java/com/study/jpabook/Board.java
+++ src/main/java/com/study/jpabook/Board.java
@@ -11,7 +11,7 @@ import javax.persistence.*;
 // name 식별자 생성기 이름 (필수)
 // sequenceName 데이터베이스에 등록되어 있는 시퀀스 이름 (hibernate_sequence)
 // initialValue DDL 생성 시에만 사용됨. 시퀀스 DDL을 생성할 떄 처음 시작하는 수를 지정한다 (1)
-// allocationSize 시퀀스 한번 호출에 증가하는 수(성능 최적화에 사용됨) 50
+// allocationSize 시퀀스 한번 호출에 증가하는 수(성능 최적화에 사용됨) 50이 기본값
 // catalog.schema 데이터베이스 catalog.schema 이름
 public class Board {
     @Id
